7d24f13efbf3748cb12743831aaeb4b384e05b36
maruf <marufilyasa13@gmail.com>
2021-03-28 09:55:15 +0700

    fit the API server to discord bot

--- bot.js
+++ bot.js
@@ -1,28 +1,41 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fetch = require('node-fetch')
-const axios = require('axios')
+const { BrainlyAPI, Server } = require('brainly-api');
 require('dotenv').config()
 
-async function getres(q){    
-    const response = await axios.get(`http://localhost:5000/api?question=${q}`)
-    return response.data.data.answer;
+async function getAns(q){
+    var res = undefined
+    await BrainlyAPI.startWorker({ experimental: true, server: Server.ID }, async brainly => {
+        const result = await brainly.findQuestion(q);
+        let array = Array.from(result)
+        array = array[0]
+        res = array.raw.node.answers.nodes[0].content.replace( /(<([^>]+)>)/ig, '')
+    });
+    return res;
 }
+
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
 client.on('message',async msg => {
-    if (/m!ask/i.test(msg.content)) {
+    let author = msg.author
+    if (/m!ask/i.test(msg.content) && author.bot === false) {
         let question = msg.content.split(' '),
             answer = null,
             q = null
+            reply = null
         question.shift()
         q = question.toString().replace(',',' ')
-        answer = await getres(q)
-        msg.reply(`\n **Question**:\n ${q} \n **Answers** \n ${answer}`);
+        answer = await getAns(q)
+        reply = `\n >>> **Question**:\n ${q} \n **Answers** \n ${answer}`
+        msg.reply(reply);
     }
+    if (/m!help/i.test(msg.content) && author.bot === false) {
+        msg.reply(`\n >>> **Command** \n -m!ask <your question> \n -m!help`)
+    }
+
 });
 
 client.login(process.env.TOKEN);
